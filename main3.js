bf7dcb17c79fed12c1a3fdf7ddaf96f349f1906f
Anna Muradova <annaermolenko2017@gmail.com>
2024-01-17 00:11:18 +0200

    New files hw-3

---
+++ main3.js
@@ -0,0 +1,9 @@
+let firstNumber = +prompt('Введите число от 0 до 100!');
+
+let secondNumber = +prompt('Введите число от 100 до 1000!');
+
+let thirdNumber = +prompt('Введите число от 1000 до 10000!');
+
+let result = (firstNumber + secondNumber + thirdNumber) / 3;
+
+alert(result);
