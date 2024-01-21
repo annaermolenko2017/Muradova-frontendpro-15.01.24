d250feb978795bc83ea49248f69fdeb673b6a4e9
Anna Muradova <annaermolenko2017@gmail.com>
2024-01-22 00:00:11 +0200

    Added changes main.js

--- main4.js
+++ main4.js
@@ -1,4 +1,22 @@
-let num1 = +prompt('Введіть число 2!');
+//Друга версія
+let number1 = +prompt('Введіть цифру 2!');
+let number2 = +prompt('Введіть цифру 2!');
+
+//Алертом виводиться по черзі
+// alert(`${number1} + ${number2} = ${number1 + number2}`);
+// alert(`${number1} - ${number2} = ${number1 - number2}`);
+// alert(`${number1} * ${number2} = ${number1 * number2}`);
+// alert(`${number1} / ${number2} = ${number1 / number2}`);
+
+//У консолі видно всі результати разом
+console.log(`${number1} + ${number2} = ${number1 + number2}`);
+console.log(`${number1} - ${number2} = ${number1 - number2}`);
+console.log(`${number1} * ${number2} = ${number1 * number2}`);
+console.log(`${number1} / ${number2} = ${number1 / number2}`);
+
+
+//Перша версія
+/*let num1 = +prompt('Введіть число 2!');
 let num2 = +prompt('Введіть число 2!');
 let sign = prompt('Введіть одну з цих операцій: + - * /');
 
@@ -18,4 +36,4 @@ switch (sign) {
         break;
     default :
         alert('Ви ввели не знак!!!');
-}
+}*/
