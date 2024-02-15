7380d33ebb9a973a17180e74d0d4c6b34d581011
Anna Muradova <annaermolenko2017@gmail.com>
2024-02-16 01:06:27 +0200

    Added new files hw-19

---
+++ main19.js
@@ -0,0 +1,17 @@
+//Отримати від користувача 3 рядки та вивести їх у довільному порядку однією командою (конкатенація);
+
+/*let strings = [];
+
+strings.push(prompt("Введіть перше слово!"));
+strings.push(prompt("Введіть друге слово!"));
+strings.push(prompt("Введіть третє слово!"));
+
+console.log(strings.sort(() => Math.random() > 0.5 ? 1 : -1).join(" "));*/
+
+//Розбити за цифрами п'ятизначне число і вивести у вихідному порядку через пробіл.
+/*function numbersBySpaces(number) {
+    console.log([...String(number)].join(' '));
+}
+
+const myNumber = 98765;
+numbersBySpaces(myNumber);*/
